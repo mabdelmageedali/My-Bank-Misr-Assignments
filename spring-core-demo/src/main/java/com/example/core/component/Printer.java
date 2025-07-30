45321b8080e9ca2c2b0159399e9b66e131309634
mabdelmageedali <142686553+mabdelmageedali@users.noreply.github.com>
2025-07-30 17:43:36 +0300

    Delete spring-core-demo directory

--- spring-core-demo/src/main/java/com/example/core/component/Printer.java
+++
@@ -1,9 +0,0 @@
-package com.example.core.component;
-import org.springframework.stereotype.Component;
-
-@Component
-class Printer {
-    public void print(String message) {
-        System.out.println(message);
-    }
-}
